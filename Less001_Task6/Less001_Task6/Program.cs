ceba5480e915e00251387ccb18ccb9ab4c257d32
Олександр Дибач <alex.dybach.2014@i.ua>
2020-10-27 13:43:24 +0200

    Done lesson 1 task 6

---
+++ Less001_Task6/Less001_Task6/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Less001_Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isMarried = false;
+            Console.WriteLine($"IsMarried: {isMarried}");
+        }
+    }
+}
